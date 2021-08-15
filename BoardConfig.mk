45fc5921ee5ad8a6f30a3a5622de2579f823399e
MD Raza <raza231198@gmail.com>
2021-08-15 18:36:14 +0530

    guamp: set TARGET_RECOVERY_FSTAB

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,3 +97,4 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/twrp.fstab
